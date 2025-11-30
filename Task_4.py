a20545f3c8d0335e02a112b46a174de3552affa8
sky007-SM <niranjanunnikrishnan2007@gmail.com>
2025-11-30 13:09:03 +0530

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,37 +1,24 @@
 #program to illustrate various set operations
-Set1=[1,2,3,4,5]
-Set2=[3,4,8,9,10]
+Set1={1,2,3,4,5}
+Set2={3,4,8,9,10}
 print("Set1: ",Set1)
 print("Set2: ",Set2)
 #Union of Set1 and Set2
-Union=[]
-for i in Set1:
-    if i not in Union:
-        Union.append(i)
-for i in Set2:
-    if i not in Union:
-        Union.append(i)
+Union=Set1.union(Set2)
 print("Union of Set1 and Set2: ",Union)
 #Intersection of Set1 and Set2
-Intersection=[]
-for i in Set1:
-    if i in Set2:
-        Intersection.append(i)
+Intersection=Set1.intersection(Set2)
 print("Intersection of Set1 and Set2: ",Intersection)
 #Difference of Set1 and Set2
-Difference=[]
-for i in Set1:
-    if i not in Set2:
-        Difference.append(i)
+Difference=Set1.difference(Set2)
 print("Difference of Set1 and Set2: ",Difference)
 #Symmetric difference of Set1 and Set2
-SymmetricDifference=[]
-for i in Set1:
-    if i not in Set2:
-        SymmetricDifference.append(i)
-for i in Set2:
-    if i not in Set1:
-        SymmetricDifference.append(i)
+SymmetricDifference=Set1.symmetric_difference(Set2)
 print("Symmetric difference of Set1 and Set2: ",SymmetricDifference)
+
+
+
+
+
 
 
